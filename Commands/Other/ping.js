58383b94a890c69a717e7fd15569c5f35a60131a
Jan <139629565+intergered@users.noreply.github.com>
2024-09-25 00:41:45 +0100

    Create ping.js

---
+++ Commands/Other/ping.js
@@ -0,0 +1,15 @@
+const {
+  SlashCommandBuilder,
+  CommandInteraction,
+  PermissionFlagsBits,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Pong")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  execute(interaction) {
+    interaction.reply({ content: `🏓 - Pong`, ephemeral: true });
+  },
+};
