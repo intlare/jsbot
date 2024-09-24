000244b14dd7413fdc36531daa11aaa45170f499
Jan <139629565+intergered@users.noreply.github.com>
2024-09-25 00:38:20 +0100

    Create commandHandler.js

---
+++ Handlers/commandHandler.js
@@ -0,0 +1,31 @@
+function loadCommands(client) {
+  const ascii = require("ascii-table");
+  const fs = require("fs");
+  const table = new ascii().setHeading("Commands", "Status");
+
+  let commandsArray = [];
+
+  const commandsFolder = fs.readdirSync(`./Commands`);
+  for (const folder of commandsFolder) {
+    const commandFiles = fs
+      .readdirSync(`./Commands/${folder}`)
+      .filter((file) => file.endsWith(`.js`));
+
+    for (const file of commandFiles) {
+      const commandFile = require(`../Commands/${folder}/${file}`);
+
+      client.commands.set(commandFile.data.name, commandFile);
+
+      commandsArray.push(commandFile.data.toJSON());
+
+      table.addRow(file, "loaded");
+      continue;
+    }
+  }
+
+  client.application.commands.set(commandsArray);
+
+  return console.log(table.toString(), "\n✅ - Loaded Commands");
+}
+
+module.exports = { loadCommands };
